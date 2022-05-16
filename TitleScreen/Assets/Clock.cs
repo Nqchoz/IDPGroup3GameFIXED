a1dbc0202f8b97454391fd983f42dc2add53382c
Nqchoz <joshpham15@gmail.com>
2022-05-16 15:42:30 -0700

    V1.2 WORKING TIMER
    all i did was add the timer

--- TitleScreen/Assets/Clock.cs
+++ TitleScreen/Assets/Clock.cs
@@ -6,20 +6,43 @@ using TMPro;
 
 public class Clock : MonoBehaviour
 {
-    public Text timerText;
-    public int time;
-
+    public TextMeshProUGUI timerText;
+    public float timetodisplay;
+    public bool timerpower;
+    
     // Start is called before the first frame update
     void Start()
     {
-        time = 0;
+        timetodisplay = 30*60;    
+        timerpower = true;
     }
 
+    void countdown()
+    {
+        timetodisplay -= Time.deltaTime;
+        float minutes = Mathf.FloorToInt(timetodisplay / 60); 
+        float seconds = Mathf.FloorToInt(timetodisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
     // Update is called once per frame
     void Update()
     {
+        if (timerpower){
+
+                if (timetodisplay >= 0){    
+                countdown();
+
+            }
+
+            if(timetodisplay<0){
+
+                Debug.Log("Time has run out!");
+                timerpower = false;
+
+            }
+
+        }
         
-        time += Mathf.RoundToInt(Time.deltaTime);
-        timerText.text = time.ToString();
     }
 }
