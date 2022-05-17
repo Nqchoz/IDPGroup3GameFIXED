f76b38b67d506323ff9b82f129899a8694221294
Nqchoz <joshpham15@gmail.com>
2022-05-16 19:07:56 -0700

    V1.6 Settings menu
    settings menu opens/closes and timer pauses and resumes accordingly

---
+++ TitleScreen/Assets/Scripts/SettingsMEnu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMEnu : MonoBehaviour
+{
+    
+    public Canvas canvas;
+    public bool showing = false;
+    public GameObject obj;
+
+    void Awake(){
+        obj = GameObject.FindGameObjectWithTag ("Timer");
+
+    }
+
+
+    public void popup(){
+
+        if (showing == false){
+
+            showing = true;
+            canvas.enabled = showing;
+            obj.GetComponent<Clock> ().timerpower = false;
+        }
+
+        else if (showing == true){
+
+            showing = false;
+            canvas.enabled = showing;
+            obj.GetComponent<Clock> ().timerpower = true;
+        }
+
+
+
+
+
+    }
+}
