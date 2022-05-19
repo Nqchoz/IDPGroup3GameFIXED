e357471e8da511399c2c975938cc02879dfdfe23
Nqchoz <joshpham15@gmail.com>
2022-05-18 17:34:24 -0700

    v1.7.1
    still working on the click functionality

--- TitleScreen/Assets/Scripts/ShelfClicked.cs
+++ TitleScreen/Assets/Scripts/ShelfClicked.cs
@@ -6,15 +6,19 @@ public class ShelfClicked : MonoBehaviour
 {
 
     public GameObject camera;
+    public GameObject GameUI;
 
     void Awake(){
         camera = GameObject.FindGameObjectWithTag("MainCamera");
+        GameUI = GameObject.FindGameObjectWithTag("GUI");
+        
     }
 
-    void OnMouseDown()
+    public void Shelf_1Zoom()
     {
         Debug.Log("Registered Click");
         camera.transform.position += new Vector3(0, 1920.0f, 0);
+        GameUI.transform.position += new Vector3(0, 1920.0f, 0);
         
     }
 
