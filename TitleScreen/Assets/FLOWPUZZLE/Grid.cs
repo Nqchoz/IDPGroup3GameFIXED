d894c2e2fefebdc0df4c60e84db9719097db959d
Nqchoz <joshpham15@gmail.com>
2022-05-25 00:07:52 -0700

    v1.8.3
    started working on the flow puzzle

---
+++ TitleScreen/Assets/FLOWPUZZLE/Grid.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public enum Colors{
+        Empty,
+        Red,
+        Yellow,
+        Blue,
+        Green,
+        Orange
+
+    }
+   public int[,] gameboard = {{3,1,1,1,5},{3,1,2,2,5},{3,1,2,5,5},{3,1,5,5,4},{3,3,4,4,4}}; 
+   public int[,] winboard = {{3,0,0,1,5},{0,0,0,2,0},{0,0,2,0,0},{0,1,5,0,4},{0,3,4,0,0}};
+
+  
+}
