1cd9450ffb445f4fb34e5299f939a67cc5ad9845
Nqchoz <joshpham15@gmail.com>
2022-05-25 12:55:09 -0700

    v1.8.3.5
    Continued working on flow puzzle

--- TitleScreen/Assets/FLOWPUZZLE/Grid.cs
+++ TitleScreen/Assets/FLOWPUZZLE/Grid.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Grid : MonoBehaviour
 {
@@ -13,8 +14,10 @@ public class Grid : MonoBehaviour
         Orange
 
     }
-   public int[,] gameboard = {{3,1,1,1,5},{3,1,2,2,5},{3,1,2,5,5},{3,1,5,5,4},{3,3,4,4,4}}; 
-   public int[,] winboard = {{3,0,0,1,5},{0,0,0,2,0},{0,0,2,0,0},{0,1,5,0,4},{0,3,4,0,0}};
+   public int[,] winboard = {{3,1,1,1,5},{3,1,2,2,5},{3,1,2,5,5},{3,1,5,5,4},{3,3,4,4,4}}; 
+   public int[,] gameboard = {{3,0,0,1,5},{0,0,0,2,0},{0,0,2,0,0},{0,1,5,0,4},{0,3,4,0,0}};
+   public GameObject[,] board;
+   public GameObject[] linePrefs;
 
   
 }
