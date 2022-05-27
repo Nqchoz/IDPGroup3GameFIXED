6fd3727df800f08eee4a5d305c37b2d92af6db1a
Nqchoz <joshpham15@gmail.com>
2022-05-26 17:49:20 -0700

    v1.8.5.7
    Continued working on dialogue

---
+++ TitleScreen/Assets/Images/Dialogue/Dialogue.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+    public GameObject DialogueGroup;
+    
+    void Awake(){
+        continueButton = GameObject.Find("Continue");
+        textDisplay = GameObject.Find("DialogueText").GetComponent<TextMeshProUGUI>();
+        DialogueGroup = GameObject.Find("Dialogue");
+    }
+    public void DoDialogue(){
+        StartCoroutine(Type());
+    }
+
+    void Update(){
+        if(textDisplay.text == sentences[index]){
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type(){
+        foreach(char letter in sentences[index].ToCharArray()){
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence(){
+        continueButton.SetActive(false);
+        if (index < sentences.Length - 1){
+            index ++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+
+        }
+        else{
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+
+    }
+
+}
